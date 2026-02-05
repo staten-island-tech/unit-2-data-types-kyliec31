0249bb3b121349bd97fba80b2e293dd0f5c9964b
kyliec31 <kyliec31@nycstudents.net>
2026-02-05 09:26:16 -0500

    2/5

---
+++ unit2test.py
@@ -0,0 +1,70 @@
+""" x = 3
+y = float(3)
+print(x,y) """
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i) """
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(values[0])
+    print(values[6]) """
+
+""" values = [1,2,5,7]
+
+print(values)
+for i in values:
+    print(values[0])
+ """
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+""" 
+sentence = input("Input a sentence") """
+
+""" day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect")
+ """
+""" def discount(age, isMember, isResident):
+    if age <= 12 or age >=65:
+        print("discount")
+
+    elif isMember == True:
+        print("discount")
+    elif isResident == True:
+        print("discount")
+    else:
+        print("no discount")
+discount(30, False, False) """
+
+""" x = "test"
+print(f"hello {x}") """
+""" 
+temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold') """
+
+""" number = int(input("Input a number - "))
+if int(number) % 2 == 0:
+        print("even")
+else:
+        print("odd") """
+
+a = "0%, 15%, 20%, 25%"
+bill = a
+x = "bad, okay, good, great"
+service = x
+y = x.split(0)
+if x[0] == y[0]:
